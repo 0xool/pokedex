7cc1d18fd91538d5ab1840c912b765ac23fb6797
CyrusREfahi <CyrusRefahi@cyruss-MacBook-Pro.local>
2016-05-11 16:16:35 +0430

    added a button for audioplayer and added audio player alos completed connecting cvs to model to view

--- pockedex/ViewController.h
+++ pockedex/ViewController.h
@@ -9,15 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "Pockemon.h"
 #import "csvParser.h"
+#import <AVFoundation/AVFoundation.h>
+
 
 @interface  ViewController : UIViewController <UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout , NSObject>
 
 
 @property (weak , nonatomic) IBOutlet UICollectionView* collectionView;
 @property (weak , nonatomic)   Pockemon* pokemon;
+@property  NSMutableArray* pokemons;
+@property (weak , nonatomic) AVAudioPlayer* MusicPlayer;
 
 
+- (IBAction)musicButtonPressed:(id)sender;
 -(void)parsePokemonCSV;
+-(void)initAudio;
 
 
 
--- pockedex/ViewController.m
+++ pockedex/ViewController.m
@@ -12,16 +12,25 @@
 
 @interface  ViewController ()
 
+
+
 @end
 
-@implementation ViewController 
+@implementation ViewController
+{
+    AVAudioPlayer *audioPlayer;
+}
+    
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
-    
     [self parsePokemonCSV];
+    [self initAudio];
+
     
 }
 
@@ -30,31 +39,63 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)initAudio{
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"music" ofType:@"mp3"];
+    audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path]error:nil];
+    
+    audioPlayer.delegate = self;
+    [audioPlayer play];
+    NSLog(@"done");
+    
+}
+
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     
     PokeCellCollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PokeCellCollectionViewCell" forIndexPath:indexPath];
-    Pockemon* pokemon = [[Pockemon alloc]initWithName:@"Test" withPokedexId:(int)(indexPath.row + 1)];
-
+    Pockemon* pokemon = [[Pockemon alloc]initWithName:[[self.pokemons objectAtIndex:(int)indexPath.row ] _name] withPokedexId: [[self.pokemons objectAtIndex:indexPath.row ] _pockedexID] ];
     [cell configureCell:pokemon];
     
     return cell;
 }
 
+- (IBAction)musicButtonPressed:(id)sender {
+    
+    if(audioPlayer.playing){
+        [audioPlayer stop];
+        [sender setAlpha:0.2];
+    }else{
+        [audioPlayer play];
+        [sender setAlpha:1];
+    }
+    
+    
+    
+}
+
 -(void)parsePokemonCSV{
     
     NSBundle* path = [[NSBundle mainBundle] pathForResource:@"pokemon" ofType:@"csv"];
-    
-    //NSArray *arrayOfArrays = [CSVParser parseCSVIntoArrayOfArraysFromFile:path
-      //                                       withSeparatedCharacterString:@","
-        //quoteCharacterString:nil];
-;
+    self.pokemons = [[NSMutableArray alloc] init];
     
     NSString *file = [[NSBundle mainBundle] pathForResource:@"USPresident Wikipedia" ofType:@"csv"];
-    NSArray* array = [CSVParser parseCSVIntoArrayOfDictionariesFromFile:path
+    NSMutableArray* array = [CSVParser parseCSVIntoArrayOfDictionariesFromFile:path
                           withSeparatedCharacterString:@","
                                   quoteCharacterString:nil
                                              ];
+    for (int i = 0 ; i < array.count; i++) {
+
+        NSDictionary* tempDic = [array objectAtIndex:i];
+        
+        NSInteger pokeID = [[tempDic objectForKey:@"id"] integerValue] ;
+        NSString* pokeName = [tempDic objectForKey:@"identifier"] ;
+
+       Pockemon* tempPockemon = [[Pockemon alloc] initWithName:pokeName withPokedexId:pokeID];
+
+       [self.pokemons addObject:tempPockemon];
+    }
     
+
 
     
 }
@@ -66,11 +107,13 @@
 }
 
 
+
+
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     
     
     
-    return 718;
+    return self.pokemons.count;
 
 }
 
